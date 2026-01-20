99915eb934a82040f73dc0c443e693f2fc1ff1c1
cita <giacomomastrone@gmail.com>
2026-01-20 14:05:35 +0100

    Implement Contatto class with corrected fields
    Fix variable naming and data type issues in Contatto class.

---
+++ Contatto.java
@@ -0,0 +1,27 @@
+public class Contatto{
+  private String Nome;
+  private String Cognome;
+  private string Telefono;
+
+public Contatto(String Nome,String Cognome, String Telefono){
+  this.nome=nome;
+  this.cognome=cognome;
+  this.telefono=telefono;
+}
+  public String getNome(){
+    return Nome;
+  }
+
+  public String getCognome(){
+    return Cognome;
+  }
+
+  public String getTelefono(){
+    return Telefono
+  }
+
+  public String toString(){
+    return "Nome: " + nome + ", Telefono: ";
+  }
+}
+  
